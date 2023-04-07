b4a1dc1e7b2b52aba44172b23d53ab68fb062620
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-07 17:19:03 +0000

    Fri, Apr 7, 2023, 10:49 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,1 +1,5 @@
-input('')
+x = input('What is the Answer to the Great Question of Life, the Universe and Everything? ')
+if x == 42 or x == 'forty two'.lower() or x == 'forty-two'.lower():
+    print('Yes')
+else:
+    print('No')
