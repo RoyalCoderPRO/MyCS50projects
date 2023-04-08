6460cd57e21c917307451138a995a62dd40e0bbe
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 14:50:58 +0000

    Sat, Apr 8, 2023, 8:20 PM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,11 +1,11 @@
-x = {}
+groceries = {}
 while True:
     try:
         x = input().upper()
+        i = 1
         if x in groceries:
-
-        else:
-            groceries.append(x)
+            i += 1
+        groceries[x] = i
     except EOFError:
         break
 groceries = sorted(groceries)
