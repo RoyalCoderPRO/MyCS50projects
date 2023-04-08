b9963ce1ebaff86487c81d3113ce545a220e7f15
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 12:12:04 +0000

    Sat, Apr 8, 2023, 5:42 PM +05:30

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,2 +1,2 @@
 x = input('Write fruit here: ').lower()
-Apple, Avocado, Banana, Cantaloupe, Grapefruit, Grapes, Honeydew Melon, Kiwifruit, Lemon
+{Apple, Avocado, Banana, Cantaloupe, Grapefruit, Grapes, Honeydew Melon, Kiwifruit, Lemon, Lime, Nectarine, Orange, Peach, Pear, Pineapple, Plums, Strawberries, Sweet Cherries, Tangerine, Watermelon}
