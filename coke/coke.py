8bb827ea20afa33f31e8d6376bb7c2412fb61663
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 11:12:35 +0000

    Sat, Apr 8, 2023, 4:42 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -1,11 +1,11 @@
 x = 0
 a_p = 0
-while a_p <= 50:
-    print('Amount due: ' + str(50 - a_p))
-    x = input('Insert coin: ')
+while a_p < 50:
+    print('Amount Due: ' + str(50 - a_p))
+    x = int(input('Insert Coin: '))
     if x != 5 and x != 10 and x != 25:
         continue
     else:
         a_p += x
-if a_p > 50:
+if a_p >= 50:
     print('Change Owed: ' + str(a_p - 50))
