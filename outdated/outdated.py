71adaadc40166ee2ee46d39e6a13d27e3327b1b3
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 16:37:52 +0000

    Sat, Apr 8, 2023, 10:07 PM +05:30

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,5 +18,9 @@ while True:
         m, d, y = x.split('/')
         d, m = d.zfill(2), m.zfill(2)
         print(f'{y}-{m}-{d}')
+        break
     elif ',' in x:
-        
+        m, d, y = x.split()
+        d.replace(',','')
+        d, m = d.zfill(2), m.zfill(2)
+        break
