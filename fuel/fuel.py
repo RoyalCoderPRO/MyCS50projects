3567a5c3cb78d9cccb370679102a81767ee9e818
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 13:10:02 +0000

    Sat, Apr 8, 2023, 6:40 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,16 +1,17 @@
+import math
 while True:
     try:
         x, y = input('Fraction: ').split('/')
         z = int(x)/int(y)
         assert z <= 1
-        if z< 0.01:
+        if z <= 0.01:
             print('E')
             break
         elif z >= 0.99:
             print('F')
             break
         else:
-            print(str(int(z*100)) + '%')
+            print(str(int(math.ceil(z*100))) + '%')
             break
     except (ValueError, ZeroDivisionError, AssertionError):
         pass
