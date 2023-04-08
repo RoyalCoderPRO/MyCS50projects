f3eb994a32f42e181acc9e21baa13890fa514bb6
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 08:16:03 +0000

    Sat, Apr 8, 2023, 1:46 PM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,12 +1,18 @@
 def main():
-    if 7 <= convert(input('What time is it? ')) <= 8:
+    x = convert(input('What time is it? '))
+    if 7 <= x <= 8:
         print('breakfast time')
-    elif 12 <= convert()
+    elif 12 <= x <= 13:
+        print('lunch time')
+    elif 18 <= x <= 19:
+        print('dinner time')
+    else:
+        pass
 
 
 
 def convert(time):
-    hour, minutes = time.split(:)
+    hour, minutes = time.split(':')
     hour, minutes = int(hour), int(minutes)
     x = hour + (minutes/60)
     return x
