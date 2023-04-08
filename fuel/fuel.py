7e3b00491612a151532fc54e5456e2d72992d0fc
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 12:54:52 +0000

    Sat, Apr 8, 2023, 6:24 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,1 +1,8 @@
-x = input('Fraction: ')
+while True:
+    try:
+        x, y = input('Fraction: ').split('/')
+        print(int(x)/int(y))
+    except (ValueError, ZeroDivisionError):
+        pass
+
+
