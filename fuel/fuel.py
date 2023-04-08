6dca6e48628bc49edcdb49ec7ced4f34777e2925
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 12:58:57 +0000

    Sat, Apr 8, 2023, 6:28 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,16 @@
-try:
-    x, y = input('Fraction: ').split('/')
-    print(int(x)/int(y))
-except (ValueError, ZeroDivisionError):
-    pass
+while True:
+    try:
+        x, y = input('Fraction: ').split('/')
+        z = int(x)/int(y)
+        if z< 0:
+            print('E')
+            break
+        elif z> 99:
+            print('F')
+            break
+        else:
+            print(str(z) + '%')
+    except (ValueError, ZeroDivisionError):
+        pass
 
 
