16ed4befb3103da3fafc1e16a089e9122282c421
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 08:12:04 +0000

    Sat, Apr 8, 2023, 1:42 PM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,13 @@
 def main():
-    convert()
+    if convert(input('What time is it? ')) <:
+
 
 
 def convert(time):
-
+    hour, minutes = time.split(:)
+    hour, minutes = int(hour), int(minutes)
+    x = hour + (minutes/60)
+    return x
 
 
 if __name__ == "__main__":
