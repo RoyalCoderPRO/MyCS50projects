ed34d8623c1481a7fefde858cdb2481ab14df5b0
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 12:00:27 +0000

    Sat, Apr 8, 2023, 5:30 PM +05:30

--- plates/plates.py
+++ plates/plates.py
@@ -19,8 +19,9 @@ def is_valid(s):
             return False
         if number_mode == True and char.isalpha():
             return False
-    if integers[0] == '0':
-        return False
+    if number_mode == True:
+        if integers[0] == '0':
+            return False
     if s[0].isalpha() and s[1].isalpha() and 2 <= len(s) <= 6:
         return True
 
