eb1c857c8471b881e746e9ffed80cde4bb2f8a15
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 13:00:36 +0000

    Sat, Apr 8, 2023, 6:30 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,7 +2,7 @@ while True:
     try:
         x, y = input('Fraction: ').split('/')
         z = int(x)/int(y)
-        if z< 0:
+        if z< 1:
             print('E')
             break
         elif z> 99:
