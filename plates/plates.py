772c14739d7231c5ba4477b8f15a6f03244ef79c
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 11:24:39 +0000

    Sat, Apr 8, 2023, 4:54 PM +05:30

---
+++ plates/plates.py
@@ -0,0 +1,13 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    return s[0].isalpha() and s[1].isalpha()
+
+
+main()
