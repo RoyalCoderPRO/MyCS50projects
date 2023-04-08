9da592dd27988a88af9594bb063b218abdc0214f
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 16:54:09 +0000

    Sat, Apr 8, 2023, 10:24 PM +05:30

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,15 +15,19 @@ months = [
 
 while True:
     x = input()
-    if '/' in x:
-        m, d, y = x.split('/')
-        d, m = d.zfill(2), m.zfill(2)
-        print(f'{y}-{m}-{d}')
-        break
-    elif ',' in x:
-        m, d, y = x.split()
-        d.replace(',','')
-        m = months.index(m)+1
-        d, m = str(d).zfill(3), str(m).zfill(2)
-        print(f'{y}-{m}-{d}')
-        break
+    try:
+        if '/' in x:
+            m, d, y = x.split('/')
+            d, m = d.zfill(2), m.zfill(2)
+            print(f'{y}-{m}-{d}')
+            break
+        elif ',' in x:
+            m, d, y = x.split()
+            d.replace(',','')
+            m = months.index(m)+1
+            d, m = str(d).zfill(3), str(m).zfill(2)
+            print(f'{y}-{m}-{d}')
+            break
+        else:
+            pass
+
