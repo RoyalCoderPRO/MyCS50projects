8611161b2de999f12a0d2ae9befe75c5b74b8cb9
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-08 13:07:19 +0000

    Sat, Apr 8, 2023, 6:37 PM +05:30

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,16 +2,17 @@ while True:
     try:
         x, y = input('Fraction: ').split('/')
         z = int(x)/int(y)
+        assert z <= 1
         if z< 0.01:
             print('E')
             break
-        elif z> 0.99:
+        elif z >= 0.99:
             print('F')
             break
         else:
             print(str(int(z*100)) + '%')
             break
-    except (ValueError, ZeroDivisionError):
+    except (ValueError, ZeroDivisionError, AssertionError):
         pass
 
 
