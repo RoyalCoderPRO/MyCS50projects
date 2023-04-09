156adea51a570533664c7f47a73144ce06745fb3
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 12:05:23 +0000

    Sun, Apr 9, 2023, 5:35 PM +05:30

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,15 +1,20 @@
 import requests
 import sys
 
-n = sys.argv[1]
 try:
+    n = sys.argv[1]
     assert float(n)
-except:
-    sys.exit('Invalid Input')
+
+except AssertionError:
+    sys.exit('Command-line argument is not a number')
+except IndexError:
+    sys.exit('Missing Command-line input')
+
 try:
     data = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json',)
 except requests.RequestException:
     sys.exit('Invalid Input')
+
 price = float(data.json()['bpi']['USD']['rate_float'])
 total = price*float(n)
 print(f'${total}')
