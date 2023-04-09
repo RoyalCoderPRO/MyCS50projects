40fb36523256664096e123643283deb2e40cc402
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 06:53:57 +0000

    Sun, Apr 9, 2023, 12:23 PM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,15 +2,16 @@ from pyfiglet import Figlet
 import sys
 
 x = input('Input: ')
-if len(sys.argv) == 1:
-    font_style = 'big'
-
 try:
-    if sys.argv[1] == '-f' or sys.argv[1] == '--font':
+
+    if len(sys.argv) == 1:
+        font_style = 'big'
+    elif sys.argv[1] == '-f' or sys.argv[1] == '--font':
         font_style = sys.argv[2]
-except (ValueError, IndexError):
+except (ValueError, IndexError, NameError):
     sys.exit()
 
+print('Output: ')
 f = Figlet(font=font_style)
 print(f.renderText(x))
 
