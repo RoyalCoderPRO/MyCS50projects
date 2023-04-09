c085f3baf532ccca9f2c5a24e49aedfb55c8e995
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 10:07:41 +0000

    Sun, Apr 9, 2023, 3:37 PM +05:30

--- game/game.py
+++ game/game.py
@@ -1,8 +1,18 @@
 import random as rand
 
 while True:
-    x = input('Level: ')
+    x = int(input('Level: '))
     if 1 <= x <= 100:
         break
     else:
         pass
+y = rand.randint(1,x)
+while True:
+    z = int(input('Guess: '))
+    if z < y:
+        print('Too small!')
+    if z == y:
+        print('Just right!')
+        break
+    if z > y:
+        print('Too large!')
