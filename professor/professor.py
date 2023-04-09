bc61699bb6e3e7222322226ad0af9181f25579c6
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 11:20:13 +0000

    Sun, Apr 9, 2023, 4:50 PM +05:30

--- professor/professor.py
+++ professor/professor.py
@@ -7,16 +7,22 @@ while True:
         break
     except AssertionError:
         pass
-x = random.randint(0, 10**(l-1))
-y = random.randint(0, 10**(l-1))
-print(str(x) +' + ' + str(y))
-z = x + y
-ans = int()
-tries = 1
-while tries <= 3 and ans != z:
-    tries += 1
-    ans = int(input())
+counter = 0
+score = 0
+while counter <= 10:
+    x = random.randint(0, 10**(l-1))
+    y = random.randint(0, 10**(l-1))
+    print(str(x) +' + ' + str(y))
+    z = x + y
+    ans = int()
+    tries = 1
+    while tries <= 3 and ans != z:
+        tries += 1
+        ans = int(input())
+        if ans != z:
+            print('EEE')
     if ans != z:
-        print('EEE')
-if ans != z:
-    print(z)
+        print(z)
+    else:
+        score += 1
+    counter += 1
