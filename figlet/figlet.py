ed5bfe5f71d1fae02a2a05ed9de1cc3c97b17503
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 07:02:29 +0000

    Sun, Apr 9, 2023, 12:32 PM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,16 +1,16 @@
 from pyfiglet import Figlet
 import sys
-
+figlet = Figlet()
 x = input('Input: ')
 try:
-
     if len(sys.argv) == 1:
         font_style = 'big'
     elif sys.argv[1] == '-f' or sys.argv[1] == '--font':
         font_style = sys.argv[2]
-except (ValueError, IndexError, NameError):
+        assert font_style in figlet.getFonts()
+except (ValueError, IndexError, NameError, AssertionError):
     sys.exit()
-Figlet.getFonts()
+
 print('Output: ')
 f = Figlet(font=font_style)
 print(f.renderText(x))
