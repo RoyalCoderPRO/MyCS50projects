9ead6957e07c6cd7ad2f5e9ba81d34d8425ce0e1
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 06:48:04 +0000

    Sun, Apr 9, 2023, 12:18 PM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,4 +5,8 @@ x = input('Input: ')
 if len(sys.argv) == 1:
     f = Figlet(font='big')
     print(f.renderText(x))
-    if sys.argv[1] == '-f' or sys.argv[1] == '--font'
+
+try:
+    if sys.argv[1] == '-f' or sys.argv[1] == '--font':
+        font_style = sys.argv[2]
+
