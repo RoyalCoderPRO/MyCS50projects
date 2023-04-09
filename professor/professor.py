3a5610711bdd1da135dcc7e37a5c303b894e919b
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 11:16:39 +0000

    Sun, Apr 9, 2023, 4:46 PM +05:30

--- professor/professor.py
+++ professor/professor.py
@@ -9,13 +9,14 @@ while True:
         pass
 x = random.randint(0, 10**(l-1))
 y = random.randint(0, 10**(l-1))
+print(str(x) +' + ' + str(y))
 z = x + y
-
+ans = int()
 tries = 1
 while tries <= 3 and ans != z:
     tries += 1
-    ans = input()
-    print('EEE')
+    ans = int(input())
+    if ans != z:
+        print('EEE')
 if ans != z:
     print(z)
-elif:
