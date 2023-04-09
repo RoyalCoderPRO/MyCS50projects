a0e0d7cd1f459f7ae0de739c139c4b00fca65fbc
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 11:41:24 +0000

    Sun, Apr 9, 2023, 5:11 PM +05:30

--- professor/professor.py
+++ professor/professor.py
@@ -1,6 +1,7 @@
 import random
 def main():
-    
+    generate_integer(get_level())
+
 def get_level():
     while True:
         try:
@@ -11,23 +12,26 @@ def get_level():
         else:
             print('level: ' + str(l))
             return l
-counter = 0
-score = 0
-while counter < 10:
-    x = random.randint(10**(l-1), 10**(l)-1)
-    y = random.randint(10**(l-1), 10**(l)-1)
-    print(str(x) +' + ' + str(y))
-    z = x + y
-    ans = int()
-    tries = 1
-    while tries <= 3 and ans != z:
-        tries += 1
-        ans = int(input())
+def generate_integer(l):
+    counter = 0
+    score = 0
+    while counter < 10:
+        x = random.randint(10**(l-1), 10**(l)-1)
+        y = random.randint(10**(l-1), 10**(l)-1)
+        print(str(x) +' + ' + str(y))
+        z = x + y
+        ans = int()
+        tries = 1
+        while tries <= 3 and ans != z:
+            tries += 1
+            ans = int(input())
+            if ans != z:
+                print('EEE')
         if ans != z:
-            print('EEE')
-    if ans != z:
-        print(z)
-    else:
-        score += 1
-    counter += 1
-print(score)
+            print(z)
+        else:
+            score += 1
+        counter += 1
+    print(score)
+if __name__ == "__main__":
+    main()
