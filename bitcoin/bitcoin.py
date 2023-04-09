4f4b390f6a224bfa184a166e7db24f1b2366b946
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 12:02:42 +0000

    Sun, Apr 9, 2023, 5:32 PM +05:30

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -3,11 +3,13 @@ import sys
 
 n = sys.argv[1]
 try:
-    assert float(sys.argv[1])
+    assert float(n)
 except:
     sys.exit('Invalid Input')
 try:
     data = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json',)
-    print(data.json())
 except requests.RequestException:
     sys.exit('Invalid Input')
+price = float(data.json()['bpi']['USD']['rate_float'])
+total = price*float(n)
+print(f'${total}')
