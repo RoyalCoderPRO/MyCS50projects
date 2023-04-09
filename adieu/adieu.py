ccd27e9347398f66ab44954fdf05beee4908ebb6
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 09:28:14 +0000

    Sun, Apr 9, 2023, 2:58 PM +05:30

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -2,14 +2,15 @@ names_list = []
 
 while True:
     try:
-        names_list.append(input('Name: ') + ',')
+        names_list.append(input('Name: ') + ', ')
     except EOFError:
         break
 print()
-names_list[-2].replace(',','and')
+names_list = names_list[-2].replace(',','and')
+print(names_list[-2])
 if len(names_list) > 2:
     names_list[-1].replace(',','')
 names = ''
 for name in names_list:
     names += name
-print('Adieu, adieu to, '+ name)
+print('Adieu, adieu to '+ names)
