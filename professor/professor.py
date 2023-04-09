1d0adb1250a21dc8f2e6456ff8bf8680f96f0622
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-09 11:07:10 +0000

    Sun, Apr 9, 2023, 4:37 PM +05:30

---
+++ professor/professor.py
@@ -0,0 +1,17 @@
+import random
+
+while True:
+    try:
+        l = int(input())
+        assert l == 1 or l == 2 or l == 3
+        break
+    except AssertionError:
+        pass
+x = random.randint(0, 10**(l-1))
+y = random.randint(0, 10**(l-1))
+z = x + y
+
+tries = 1
+while tries <= 3 and ans != z:
+    tries += 1
+    ans = input()
