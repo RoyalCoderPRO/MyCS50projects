78ad30c9c211709c149997ce50f5d40046ddb80d
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-26 17:05:10 +0000

    Wed, Apr 26, 2023, 10:35 PM +05:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,13 +1,10 @@
 from bank import value
 
 def test_hello():
-
     assert value('Hello There') == '$0'
 
 def test_hi():
-
     assert value('Hi There') == '$20'
-    
+
 def test_nonh():
-    
     assert value('Whats up') == '$100'
