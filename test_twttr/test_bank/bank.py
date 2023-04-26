c8fd1d236a26809c9621cc98fa8abb74ce615525
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-26 16:27:22 +0000

    Wed, Apr 26, 2023, 9:57 PM +05:30

--- test_twttr/test_bank/bank.py
+++ test_twttr/test_bank/bank.py
@@ -1,17 +1,18 @@
 
 def main():
+    value(x)
 
 
 
-def value(greeting):
-
+def value(x):
+    x.lower().split()
+    if 'hello' in x[0]:
+        print('$0')
+    elif 'h' == x[0][0]:
+        print('$20')
+    else:
+        print('$100')
 
 
 if __name__ == "__main__":
-    main()x = input('Greeting: ').lower().split()
-if 'hello' in x[0]:
-    print('$0')
-elif 'h' == x[0][0]:
-    print('$20')
-else:
-    print('$100')
+    main()
