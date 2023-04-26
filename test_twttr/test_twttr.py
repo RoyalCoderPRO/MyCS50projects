93c739d2af7e6d2f55d5c62f4e81647c171204ce
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-26 16:13:10 +0000

    Wed, Apr 26, 2023, 9:43 PM +05:30

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -2,4 +2,7 @@ from twttr import shorten
 
 def test_null():
 
-    assert shorten() == shorten()
+    assert shorten('Twitter') == 'Twttr'
+    assert shorten('TwItter') == 'Twttr'
+    assert shorten('Tw1tter') == 'Tw1ttr'
+    assert shorten('Tw,tter') == 'Tw,ttr'
