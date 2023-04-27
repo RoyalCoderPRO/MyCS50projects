eb14c241bee77e359db5c0e910a52517259cda22
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-27 06:28:34 +0000

    Thu, Apr 27, 2023, 11:58 AM +05:30

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,14 +1,14 @@
 def main():
-    print(value())
+    print('$' + str(value()))
 
 def value(greeting = 'Hello There'):
     x = greeting.lower().split()
     if 'hello' in x[0]:
-        return '$0'
+        return 0
     elif 'h' == x[0][0]:
-        return '$20'
+        return 20
     else:
-        return '$100'
+        return 100
 
 
 if __name__ == "__main__":
