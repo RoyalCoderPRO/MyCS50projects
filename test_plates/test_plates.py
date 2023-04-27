05219df405ece871ab3515b337cfe887eefd3a84
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-04-27 09:49:01 +0000

    Thu, Apr 27, 2023, 3:19 PM +05:30

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,4 +2,6 @@ from plates import is_valid
 def test():
     assert is_valid('ad2034') == True
     assert is_valid('Adit1230') == False
-    assert is_valid('')
+    assert is_valid('ad0234') == False
+    assert is_valid('0asdf') == False
+    assert is_valid('afd_df') == False
