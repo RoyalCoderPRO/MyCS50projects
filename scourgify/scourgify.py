0637c618c045dea3fb7f6855bd7a75cd7c677791
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-01 09:43:46 +0000

    Mon, May 1, 2023, 3:13 PM +05:30

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -32,11 +32,7 @@ def scourger(old, new):
     with open(old, "r") as file:
         file_colmn = csv.DictReader(file, fieldnames= ['name','house'])
         for row in file_colmn:
-            new_dict.append({'name': row})
-
-    first, last = file_colmn['name'].split(',')
-    house = file_colmn['house']
-    new_dict = {first, last, house}
+            new_dict.append({'first': row.split(',')[0], ''})
     with open (new, 'a') as file:
         file.write(new_dict)
 
