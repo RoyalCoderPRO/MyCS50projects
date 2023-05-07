5a56bdf73c1c3aa90d2eb4be270e1e921995892d
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-07 07:31:01 +0000

    Sun, May 7, 2023, 1:01 PM +05:30

---
+++ shirt/shirt.py
@@ -0,0 +1,30 @@
+import sys
+
+def main():
+    system_check(sys.argv)
+
+def system_check(argument):
+
+    if len(argument) < 3:
+        sys.exit('Too few command-line arguments')
+
+    elif len(argument) > 3:
+        sys.exit('Too many command-line arguments')
+
+    file_name = argument[1]
+
+
+    if file_name[-4:] != '.csv':
+        sys.exit('Could not read ' + file_name)
+    else:
+        try:
+            with open(file_name, "r") as file:
+                pass
+        except FileNotFoundError:
+            sys.exit('Could not read' + file_name)
+
+def shirt_changer(self):
+    
+
+if __name__ == '__main__':
+    main()
