ced3e7ead808782d60d3f7c82b8acfed7e71c7f3
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-08 17:01:22 +0000

    Mon, May 8, 2023, 10:31 PM +05:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,17 +1,14 @@
 import re
-import sys
-
 
 def main():
     print(validate(input("IPv4 Address: ")))
 
 
 def validate(ip):
-    if re.search("\.\.\.\.", ip):
+    if re.search("[0-255]+\.[0-255]+\.[0-255]+\.[0-255]+", ip):
         return True
-
-
-...
+    else:
+        return False
 
 
 if __name__ == "__main__":
