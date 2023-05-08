ef594223dbd11aa8e91802bd47949ae8f7c1e7de
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-08 10:26:37 +0000

    Mon, May 8, 2023, 3:56 PM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -15,13 +15,13 @@ int main(void)
         int whitespace_counter = height - i;
         for (int j = 0; j < whitespace_counter; j++)
         {
-            printf("  ");
+            printf(" ");
         }
         for (int h = 0; h < i; h++)
         {
             printf("#");
         }
-        printf(" ");
+        printf("  ");
         for (int h = 0; h < i; h++)
         {
             printf("#");
