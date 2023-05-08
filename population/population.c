f968321e41b6af0fe81a2b0f9f3ca2bcc8cfbb51
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-08 08:12:33 +0000

    Mon, May 8, 2023, 1:42 PM +05:30

--- population/population.c
+++ population/population.c
@@ -8,6 +8,11 @@ int main(void)
     // TODO: Prompt for end size
     int end = get_int("End size: ");
     // TODO: Calculate number of years until we reach threshold
-    
+    int counter;
+    while (counter < end){
+        int born = start / 3
+        int dead = start / 4
+        
+    }
     // TODO: Print number of years
 }
