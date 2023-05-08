09f033b307b56969bae59ec6d3579ca58a7cb006
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-08 17:10:44 +0000

    Mon, May 8, 2023, 10:40 PM +05:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -5,10 +5,12 @@ def main():
 
 
 def validate(ip):
-    if re.search(r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+", ip):
-        return True
-    else:
+    if not re.search(r"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+", ip):
         return False
+    ips = ip.split('.')
+    elif
+    else:
+        return True
 
 
 if __name__ == "__main__":
