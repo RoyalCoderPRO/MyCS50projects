daca29875f5201c007c962701a5a3237fa6d7bc5
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-08 10:13:33 +0000

    Mon, May 8, 2023, 3:43 PM +05:30

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -8,7 +8,7 @@ int main(void)
     {
         height = get_int("Height: ");
     }
-    while (height < 0 && height < 9);
+    while (height < 1 || height > 8);
 
     for (int i = 1; i <= height; i++)
     {
