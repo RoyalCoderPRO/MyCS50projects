57905eabbb02f020699e762e3be55cbaddc78d96
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-08 08:09:27 +0000

    Mon, May 8, 2023, 1:39 PM +05:30

--- population/population.c
+++ population/population.c
@@ -4,10 +4,10 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+    string start = get_string("Start size: ")
     // TODO: Prompt for end size
-
+    string end = get_string("End size: ")
     // TODO: Calculate number of years until we reach threshold
-
+    
     // TODO: Print number of years
 }
