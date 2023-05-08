ea30cbb893ee116fdb98f04a9e331c5d46c911f5
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-08 09:22:45 +0000

    Mon, May 8, 2023, 2:52 PM +05:30

--- hello/hello.c
+++ hello/hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
-
-int main(void)
+// importing packages
+int main(void) //no parameters
 {
-    string name = get_string("What's your name? ");
-    printf("hello, %s\n", name);
+    string name = get_string("What's your name? "); //asks for user name input
+    printf("hello, %s\n", name); //concatenates and prints hello and user name
 }
