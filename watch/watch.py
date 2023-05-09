f5949cb37da0e727b3eea38a1358023b55926c32
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-09 13:32:27 +0000

    Tue, May 9, 2023, 7:02 PM +05:30

--- watch/watch.py
+++ watch/watch.py
@@ -13,7 +13,7 @@ def parse(s):
     source = source[1].strip()
     source = source.strip('"')
     slug = source.split('/')[-1]
-    slug = 'https://youtu.be/embed/' + slug
+    slug = 'https://youtu.be/' + slug
     return slug
 
 if __name__ == "__main__":
