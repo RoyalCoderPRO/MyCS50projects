95e69e4debeed988bfe167d3d5bbe796a07be582
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-09 13:39:51 +0000

    Tue, May 9, 2023, 7:09 PM +05:30

---
+++ working/working.py
@@ -0,0 +1,14 @@
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    if re.search(r'/d+:/d+',s):
+        return True
+
+if __name__ == "__main__":
+    main()
