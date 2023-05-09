21d7628d2cf9861d02d3c9aedd2935b7973ec379
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-09 14:02:02 +0000

    Tue, May 9, 2023, 7:32 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -4,11 +4,17 @@
 int main(void)
 {
     long credit = get_long("Number: ");
-    printf("%s\n",card_type(credit))
+    printf("%s\n",card_type(credit));
 }
 
 string card_type(long n)
 {
     //4003600000000014
-    for (i=0;i)
+    int evens = 0;
+    for (i=1;i<16;i++)
+    {
+        evens += (n % (10*i)) - (n % 10*(i-1));
+        printf("%i", evens);
+    }
+    return 
 }
