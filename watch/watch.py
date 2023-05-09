7fa9c9aa988cdaa7305602b2c8e07cef156ab8db
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-09 13:29:38 +0000

    Tue, May 9, 2023, 6:59 PM +05:30

--- watch/watch.py
+++ watch/watch.py
@@ -6,9 +6,7 @@ def main():
 
 def parse(s):
     #Returns 'None' for no input
-    truth = re.search(".+youtube.+",s)
-    print(truth)
-    if s == None or re.search(".+youtube.+",s):
+    if s == None or not re.search(".+youtube.+",s):
         return None
     source = re.split("src=", s)
     source = re.split('"', source[1])
@@ -17,7 +15,6 @@ def parse(s):
     slug = source.split('/')[-1]
     slug = 'https://youtu.be/embed/' + slug
     return slug
-# <iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
-# or re.search(r'[youtube]+',s)
+
 if __name__ == "__main__":
     main()
