a0913a60a4495cc1d89c2495557c4960e66209fa
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-09 13:50:27 +0000

    Tue, May 9, 2023, 7:20 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long credit = get_long("Number: ");
+    printf("%s\n",card_type(credit))
+}
+
+string card_type(long n)
+{
+    long n
 }
