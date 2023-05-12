736b9ed5e7b8ed77749fae8c5ffe2c0199108a0a
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-12 06:00:10 +0000

    Fri, May 12, 2023, 11:30 AM +05:30

--- working/working.py
+++ working/working.py
@@ -6,11 +6,6 @@ def main():
 
 def convert(s):
     match = re.search(r'1?\d?:[0-5]\d+',s)
-    if match:
-        first, second = match.groups()
-        
-
-    return match
 
 if __name__ == "__main__":
     main()
