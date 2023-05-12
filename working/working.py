85826a23884a6974f08e0635cb3fbd9e706b5725
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-12 09:19:58 +0000

    Fri, May 12, 2023, 2:49 PM +05:30

--- working/working.py
+++ working/working.py
@@ -5,11 +5,12 @@ def main():
 
 
 def convert(s):
-    time_range = re.findall(r'1?\d?:[0-5]\d+',s)
+    time_range = re.findall(r'1?\d?:[0-5]\d+ .',s)
     for times in time_range:
         hour = int(times.split(':')[0])
         if hour > 12:
-            
+            raise ValueError
+
 
 
 
