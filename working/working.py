6b0c96959a9dd864727efaf98a37d1bdd1c4dd8d
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-12 09:34:01 +0000

    Fri, May 12, 2023, 3:04 PM +05:30

--- working/working.py
+++ working/working.py
@@ -7,16 +7,17 @@ def main():
 def convert(s):
     time_range = re.findall(r'1?\d?:*[0-5]*\d*\s.M',s)
     print(time_range)
+    pm_yes = False
     for times in time_range:
+        if 'AM' in times:
+            times = int(times.strip(' AM'))
+        elif 'PM' in times:
+            times = int(times.strip(' PM'))
+            pm_yes = True
         if ':' in times:
             hour = int(times.split(':')[0])
         if hour > 12:
             raise ValueError
-        if 'AM' in times:
-            hour = int(times.strip(' AM'))
-        elif 'PM' in times:
-            hour = int(times.strip(' PM'))
-            hour += 12
 
         print(hour)
 
