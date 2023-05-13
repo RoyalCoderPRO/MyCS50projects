3a3dc6a7216401d7a6e2a20e806482338ec97f31
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-13 18:18:56 +0000

    Sat, May 13, 2023, 11:48 PM +05:30

--- working/test_working.py
+++ working/test_working.py
@@ -1,4 +1,10 @@
 from working import convert
 
 def test_conversion():
-    assert convert('9:60 AM to 5:60 PM') == 
+    with pytest.raises(ValueError):
+        convert('9:60 AM to 5:60 PM')
+    with pytest.raises(ValueError):
+        convert('9 AM - 5 PM')
+    with pytest.raises(ValueError):
+        convert('09:00 AM - 17:00 PM')
+
