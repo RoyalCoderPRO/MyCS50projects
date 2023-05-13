2751e8402b7e293c2d2f5fffeace6a44ce285e0b
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-13 17:32:29 +0000

    Sat, May 13, 2023, 11:02 PM +05:30

--- working/working.py
+++ working/working.py
@@ -24,13 +24,16 @@ def convert(s):
             raise ValueError
         if pm_yes:
             hour += 12
+        hour, minute = str(hour), str(minute)
+        if len(hour) == 1:
+            hour = '0' + hour
         if minute:
             new_time_range.append(hour + ':' + minute)
         else:
-            new_time_range.append(hour)
+            new_time_range.append(hour + ':00')
 
 
-    return hour
+    return new_time_range
 
 
 
