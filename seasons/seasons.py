8988a927bae4d0d39d4f43be0a61479d6ef9f84a
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 18:22:51 +0000

    Sun, May 14, 2023, 11:52 PM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,9 +5,12 @@ def main():
     print(time_calc(input('Date of Birth: '))) #YYYY-MM-DD
 
 def time_calc(d):
+    abs = date(0000)
     try:
         time_birth = date.fromisoformat(d)
-        print(time_birth)
+        print(abs)
+        new_time = time_birth - abs
+        print(new_time)
 
     except:
         sys.exit()
