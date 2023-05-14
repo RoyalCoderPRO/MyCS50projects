f3a2844e89cca825573186ba1df91fba00c196f6
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 07:41:13 +0000

    Sun, May 14, 2023, 1:11 PM +05:30

--- um/um.py
+++ um/um.py
@@ -7,7 +7,8 @@ def main():
 
 def count(s):
     all_ums = re.findall(r' um ', s)
-    all_ums = all_ums.append(re.findall(r'^um', s))
+    all_ums.append(re.findall(r'^um', s))
+    all_ums.append(re.findall(r'/Zum', s))
     print(all_ums)
 
 if __name__ == "__main__":
