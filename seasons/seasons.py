7ed04ae31fb1a16b354cb8326e5aa52c063cf5c5
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 13:39:59 +0000

    Sun, May 14, 2023, 7:09 PM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,9 +1,16 @@
 from datetime import date
+import sys
 
 def main():
     print(time_calc(input('Date of Birth: '))) #YYYY-MM-DD
 
 def time_calc():
+    try:
+
+    except:
+        sys.exit()
+    finally:
+        
 
 
 if __name__ == "__main__":
