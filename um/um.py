cbe29c0d04355a5b062ae5ccfa1bf7698c0b74f5
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 07:49:54 +0000

    Sun, May 14, 2023, 1:19 PM +05:30

--- um/um.py
+++ um/um.py
@@ -7,13 +7,10 @@ def main():
 
 def count(s):
     counter = 0
-    all_ums = re.findall(r' um ', s)
+    all_ums = re.findall(r'\Wum\W', s)
     for um in all_ums:
         counter += 1
-    if re.search(r'^um', s):
-        counter += 1
-    if re.search(r' um\Z', s):
-        counter += 1
+
     return counter
 
 if __name__ == "__main__":
