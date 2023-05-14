509eb2cfa50364298d9dee5e279e68d04ad90f6d
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 08:22:39 +0000

    Sun, May 14, 2023, 1:52 PM +05:30

--- um/um.py
+++ um/um.py
@@ -2,10 +2,11 @@ import re
 
 
 def main():
-    print(count(input("Text: ").lower()))
+    print(count(input("Text: ")))
 
 
 def count(s):
+    s = s.lower()
     counter = 0
     all_ums = re.findall(r'\bum\b', s)
     for um in all_ums:
