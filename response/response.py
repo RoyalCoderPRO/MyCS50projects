84f8d8a81e64631207af5006dbff3b718c0c48dd
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 08:43:08 +0000

    Sun, May 14, 2023, 2:13 PM +05:30

--- response/response.py
+++ response/response.py
@@ -4,11 +4,11 @@ def main():
 
 def email_checking():
     email = input("What's your email address? ")
-    email_address = validators.email(email)
 
     try:
         email_address = validators.email(email)
-    except:
-        pass
-    return email_address
+        return 'Valid'
+    except (errors.EmptyValueError, errors.InvalidEmailError):
+        return 'Invalid'
+
 main()
