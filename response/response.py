cc91f2de5e0ee0d5db2ce4b6a665231f3f0aa69b
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 08:38:19 +0000

    Sun, May 14, 2023, 2:08 PM +05:30

--- response/response.py
+++ response/response.py
@@ -1,30 +1,20 @@
 from validator_collection import validators, checkers, errors
 
-email_address = validators.email('test@domain.dev')
-# The value of email_address will now be "test@domain.dev"
+def email_checking():
+    email = input("What's your email address?")
+    email_address = validators.email(email)
 
-email_address = validators.email('this-is-an-invalid-email')
-# Will raise a ValueError
 
-try:
-    email_address = validators.email(None)
-    # Will raise an EmptyValueError
-except errors.EmptyValueError:
-    # Handling logic goes here
-except errors.InvalidEmailError:
-    # More handlign logic goes here
+    try:
+        email_address = validators.email(email)
+    except errors.InvalidEmailError:
+        # More handlign logic goes here
 
-email_address = validators.email(None, allow_empty = True)
-# The value of email_address will now be None
+    is_email_address = checkers.is_email('test@domain.dev')
+    # The value of is_email_address will now be True
 
-email_address = validators.email('', allow_empty = True)
-# The value of email_address will now be None
+    is_email_address = checkers.is_email('this-is-an-invalid-email')
+    # The value of is_email_address will now be False
 
-is_email_address = checkers.is_email('test@domain.dev')
-# The value of is_email_address will now be True
-
-is_email_address = checkers.is_email('this-is-an-invalid-email')
-# The value of is_email_address will now be False
-
-is_email_address = checkers.is_email(None)
-# The value of is_email_address will now be False
+    is_email_address = checkers.is_email(None)
+    # The value of is_email_address will now be False
