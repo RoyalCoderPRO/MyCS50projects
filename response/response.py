6e38bde4e4a4e5669bc9296835bf54e66dfe5db7
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-14 08:41:23 +0000

    Sun, May 14, 2023, 2:11 PM +05:30

--- response/response.py
+++ response/response.py
@@ -1,20 +1,14 @@
 from validator_collection import validators, checkers, errors
+def main():
+    print(email_checking())
 
 def email_checking():
-    email = input("What's your email address?")
+    email = input("What's your email address? ")
     email_address = validators.email(email)
-
 
     try:
         email_address = validators.email(email)
-    except errors.InvalidEmailError:
-        # More handlign logic goes here
-
-    is_email_address = checkers.is_email('test@domain.dev')
-    # The value of is_email_address will now be True
-
-    is_email_address = checkers.is_email('this-is-an-invalid-email')
-    # The value of is_email_address will now be False
-
-    is_email_address = checkers.is_email(None)
-    # The value of is_email_address will now be False
+    except:
+        pass
+    return email_address
+main()
