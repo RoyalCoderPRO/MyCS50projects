2abe3b802fa8a9b2699efff8ecf5389c1f640b55
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-15 04:58:07 +0000

    Mon, May 15, 2023, 10:28 AM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -6,21 +6,19 @@ def main():
     print(time_calc(input('Date of Birth: '))) #YYYY-MM-DD
 
 
-
 def time_calc(d):
     p = inflect.engine()
     abs = date.today()
-    print(abs)
     try:
         time_birth = date.fromisoformat(d)
         new_time = timedelta()
         new_time = abs - time_birth
         new_time = new_time.total_seconds()/60
-    except:
-        sys.exit()
+    except UnboundLocalError:
+        sys.exit('Invalid date')
 
     finally:
-        new_time = p.number_to_words(int(new_time), andword="")
+        new_time = p.number_to_words(int(new_time), andword="").capitalize()
         return new_time
 
 
