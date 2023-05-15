fe8feeb47ab70176fbaf971ec38d526a717ee55e
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-15 04:55:07 +0000

    Mon, May 15, 2023, 10:25 AM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,10 +1,14 @@
 from datetime import date, timedelta
 import sys
+import inflect
 
 def main():
     print(time_calc(input('Date of Birth: '))) #YYYY-MM-DD
 
+
+
 def time_calc(d):
+    p = inflect.engine()
     abs = date.today()
     print(abs)
     try:
@@ -16,6 +20,7 @@ def time_calc(d):
         sys.exit()
 
     finally:
+        new_time = p.number_to_words(int(new_time), andword="")
         return new_time
 
 
