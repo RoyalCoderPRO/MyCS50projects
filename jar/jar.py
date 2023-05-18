97d1939922441da1bb69220d77ce2013de118894
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-18 13:24:21 +0000

    Thu, May 18, 2023, 6:54 PM +05:30

--- jar/jar.py
+++ jar/jar.py
@@ -2,6 +2,7 @@ class Jar:
     def __init__(self, capacity=12):
 
         self.capacity = capacity
+        self.size = 0
 
 
     def __str__(self):
@@ -25,6 +26,9 @@ class Jar:
     @property
     def size(self):
         return self._size
+    @size.setter
+    def size(self, size):
+        self._size = size
 
 
 
