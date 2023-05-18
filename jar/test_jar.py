0109c154eb29d8fad16ae70ddb7067c9be14814e
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-18 14:05:24 +0000

    Thu, May 18, 2023, 7:35 PM +05:30

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -4,7 +4,7 @@ from pytest import raises
 def test_jar():
     with raises(ValueError):
         bottle = Jar(12)
-        Jar.deposit(bottle, 4)
+        Jar.deposit(bottle, 15)
         print(bottle)
 
 
