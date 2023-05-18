65915b8c5edf52bec7df0bc2672cd4444deb6fb3
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-18 14:21:18 +0000

    Thu, May 18, 2023, 7:51 PM +05:30

--- jar/jar.py
+++ jar/jar.py
@@ -20,7 +20,9 @@ class Jar:
     @property
     def capacity(self):
         return self.insideproperty #The value for self.insideproperty comes from the setter
+
     @capacity.setter #This assigns 'setter' to the capacity property and runs a function
+    
     def capacity(self, capacity):
         if capacity< 0:
             raise ValueError
