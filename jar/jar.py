9ac3595dfecc9539e3d17cfeb1378afaaf9928c4
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-18 10:29:34 +0000

    Thu, May 18, 2023, 3:59 PM +05:30

--- jar/jar.py
+++ jar/jar.py
@@ -1,20 +1,31 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+
+        if int(capacity) < 0:
+            raise ValueError
+        self.capacity = capacity
+        self.size = size
+
 
     def __str__(self):
-        ...
+        return "🍪" * self.size
 
     def deposit(self, n):
-        ...
+        self.size += n
 
     def withdraw(self, n):
-        ...
+        self.size -= n
 
-    @property
-    def capacity(self):
+    #@property
+    #def capacity(self):
         ...
 
     @property
     def size(self):
-        ...
+        
+bank = Jar(3)
+print(bank.size)
+print(bank)
+Jar.deposit(bank, 3)
+print(bank.size)
+print(bank)
