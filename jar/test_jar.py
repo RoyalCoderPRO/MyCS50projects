e572ae8b5be33b5d32f83fa0fd29c16e7b02445c
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-18 14:15:33 +0000

    Thu, May 18, 2023, 7:45 PM +05:30

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,11 +1,25 @@
 from jar import Jar
 from pytest import raises
 
-def test_jar():
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
+
+def test_str():
+    jar = Jar(4)
+    jar.deposit(3)
+    assert str(jar) == "🍪🍪🍪"
+
+def test_deposit():
+    jar = Jar(3)
+    jar.deposit(3)
+    assert jar.size == jar.capacity
     with raises(ValueError):
-        bottle = Jar(12)
-        Jar.deposit(bottle, 15)
-        print(bottle)
-
-
-test_jar()
+        jar.deposit(1)
+def test_withdraw():
+    jar = Jar(5)
+    jar.deposit(4)
+    jar.withdraw(3)
+    assert jar.size == 1
+    with raises(ValueError):
+        jar.withdraw(2)
