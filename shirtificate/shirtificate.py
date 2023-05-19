00e14c3f991d27109a1a780f2d576ced9241496e
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-19 10:38:50 +0000

    Fri, May 19, 2023, 4:08 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,26 +1,5 @@
 from fpdf import FPDF
 
-class PDF(FPDF):
-    def header(self):
-        # Rendering logo:
-        self.image("../docs/fpdf2-logo.png", 10, 8, 33)
-        # Setting font: helvetica bold 15
-        self.set_font("helvetica", "B", 15)
-        # Moving cursor to the right:
-        self.cell(80)
-        # Printing title:
-        self.cell(30, 10, "Title", border=1, align="C")
-        # Performing a line break:
-        self.ln(20)
-
-    def footer(self):
-        # Position cursor at 1.5 cm from bottom:
-        self.set_y(-15)
-        # Setting font: helvetica italic 8
-        self.set_font("helvetica", "I", 8)
-        # Printing page number:
-        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
-
 name = input("Name: ")
 pdf = FPDF()
 pdf= FPDF(orientation="P", unit="mm", format="A4")
@@ -28,11 +7,7 @@ pdf.set_font("helvetica", "B", 45)
 pdf.add_page()
 pdf.cell(0,60,"CS50 Shirtificate", align = "C")
 pdf.image("shirtificate.png", x=0, y =70)
+pdf.set_font_size(30)
+pdf.set_text_color(255, 255, 255)
+pdf.text(x=45, y=150, txt=f"{name} took CS50")
 pdf.output("shirtificate.pdf")
-
-
-
-
-#The top of the PDF should say “CS50 Shirtificate” as text, centered horizontally.
-#The shirt’s image should be centered horizontally.
-#The user’s name should be on top of the shirt, in white text.
