ae8ddcc1bfb4cb5f731a917daf124c82543dd672
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-19 11:38:49 +0000

    Fri, May 19, 2023, 5:08 PM +05:30

--- project/project.py
+++ project/project.py
@@ -1,16 +1,3 @@
-def main():
-    ...
-
-def function_1():
-    ...
-
-def function_2():
-    ...
-
-def function_3():
-    ...
-if __name__ == "__main__":
-    main()
 import requests
 
 def main():
