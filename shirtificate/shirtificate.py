275cf59e9ddb45c8018eed96adf8127b915e11d5
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-19 09:39:34 +0000

    Fri, May 19, 2023, 3:09 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,13 +2,14 @@ from fpdf import FPDF
 
 name = input("Name: ")
 pdf = FPDF()
-pdf.add_page()
-pdf.cell(txt="CS50 Shirtificate", align = 'C')
 pdf(orientation="P", unit="mm", format="A4")
 pdf.output("shirtificate.pdf")
+pdf.add_page()
+pdf.cell(txt="CS50 Shirtificate", align = 'C')
 
 
-The orientation of the PDF should be Portrait.
-The top of the PDF should say “CS50 Shirtificate” as text, centered horizontally.
-The shirt’s image should be centered horizontally.
-The user’s name should be on top of the shirt, in white text.
+
+
+#The top of the PDF should say “CS50 Shirtificate” as text, centered horizontally.
+#The shirt’s image should be centered horizontally.
+#The user’s name should be on top of the shirt, in white text.
