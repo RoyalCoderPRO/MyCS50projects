856cfa26e9032c31d8eca3fa8fd12d862cb60a84
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-19 12:04:47 +0000

    Fri, May 19, 2023, 5:34 PM +05:30

--- project/project.py
+++ project/project.py
@@ -16,48 +16,46 @@ def main():
                 print('Not a pokemon, try again')
                 pass
             # enters name
-            else:
-                pokemon_name = pokemon_name.capitalize()
-                i = 0
-                file.write(f'{pokemon_name}:\n  Abilities: \n')
-                # indexes desired 'ability' and 'ability slot'
-                for ability_num in data.json()['abilities']:
-                    i += 1
-                    ability = ability_num['ability']
-                    name = ability['name']
-                    slot = ability_num['slot']
-                    name_type = []
-                    # prints into txt file
-                    file.write(f'   {i}> {name}, slots: {slot},\n')
-                file.write(f'  Types:\n')
-                j = 0
-                # indexes desired 'types'
-                for types_num in data.json()['types']:
-                    j += 1
-                    types = types_num['type']
-                    name_type = types['name']
-                    file.write(f'   {j}> {name_type}\n')
 
-                file.close
-                break
+            pokemon_name = pokemon_name.capitalize()
+            i = 0
+            file.write(f'{pokemon_name}:\n  Abilities: \n')
+            # indexes desired 'ability' and 'ability slot'
+            for ability_num in data.json()['abilities']:
+                i += 1
+                ability = ability_num['ability']
+                name = ability['name']
+                slot = ability_num['slot']
+                name_type = []
+                # prints into txt file
+                file.write(f'   {i}> {name}, slots: {slot},\n')
+            file.write(f'  Types:\n')
+            j = 0
+            # indexes desired 'types'
+            for types_num in data.json()['types']:
+                j += 1
+                types = types_num['type']
+                name_type = types['name']
+                file.write(f'   {j}> {name_type}\n')
 
-    # for repeating application task
-    repeat = input('Done! Would you like to add any other Pokemon to your Pokedex?: (Y/N) ')
+            file.close
+            break
+
+
+    repeat = repeater()
+    return repeat
+# takes return value of main and repeats if Y
+
+def repeater():
     while True:
         try:
+            repeat = input('Done! Would you like to add any other Pokemon to your Pokedex?: (Y/N) ')
             assert repeat == 'Y' or repeat == 'N'
         except:
             repeat = input("Only enter 'Y' or 'N' to answer 'Yes' or 'No':")
         else:
-            break
-
-    return repeat
-# takes return value of main and repeats if Y
-
-def inputcleaner():
-
-
- 
+            return repeat
+        # for repeating application task
 
 
 if __name__ == "__main__":
