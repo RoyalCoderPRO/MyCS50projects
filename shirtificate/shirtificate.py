bc94f0e3d64f49136e86f1ac82f5678d4c657e1c
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-19 09:28:38 +0000

    Fri, May 19, 2023, 2:58 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,3 +6,9 @@ pdf.add_page()
 pdf.set_font('helvetica', size=12)
 pdf.cell(txt="hello world")
 pdf.output("hello_world.pdf")
+
+The orientation of the PDF should be Portrait.
+The format of the PDF should be A4, which is 210mm wide by 297mm tall.
+The top of the PDF should say “CS50 Shirtificate” as text, centered horizontally.
+The shirt’s image should be centered horizontally.
+The user’s name should be on top of the shirt, in white text.
