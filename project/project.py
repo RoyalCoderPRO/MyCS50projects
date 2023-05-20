38b7095651eb7e06f7079e7882e1276d90dd80c6
RoyalCoderPRO <87411629+RoyalCoderPRO@users.noreply.github.com>
2023-05-20 06:34:05 +0000

    Sat, May 20, 2023, 12:04 PM +05:30

--- project/project.py
+++ project/project.py
@@ -21,21 +21,28 @@ def menu():
 
 def pokereader():
     with open('PokeDex.txt', mode= 'r') as file:
+        i = 0
         for line in file:
-            if line.startswith(' '):
+            if line.startswith(' '): #Only pokemon names do not start with <whitespace>
                 continue
+            i += 1
             pokemon_name = line.strip('\n').strip(':')
-            print(pokemon_name)
+            print(f'{i}> {pokemon_name}')
 
 
 
 def pokelister():
     # start editing
     with open('PokeDex.txt', mode= 'a') as file:
-        # create loop to keep appending until exit program
+
+        # create loop to keep appending until exit the file, function and program
         while True:
+
+
             # user input for pokemon name
             pokemon_name = input('Which pokemon would you like to add to pokedex?: ').lower()
+
+
             # checks for request error
             try:
                 data = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}')
@@ -45,32 +52,41 @@ def pokelister():
                 print('Not a pokemon, try again')
                 pass
             # enters name
+
+
             else:
                 pokemon_name = pokemon_name.capitalize()
 
-                file.write(f'{pokemon_name}:\n  Abilities: \n')
-                # indexes desired 'ability' and 'ability slot'
+                file.write(f'{pokemon_name}:\n  Abilities: \n')  #Writes the pokemon's name and starts ability printing loop
+
+
+                # indexes desired 'ability' and 'ability slot' in the txt file (for enumerated abilities)
                 i = 0
                 for ability in data.json()['abilities']:
                     i += 1
                     name = ability['ability']['name']
                     slot = ability['slot']
-                    name_type = []
+
                     # prints into txt file
                     file.write(f'   {i}> {name}, slots: {slot},\n')
                 file.write(f'  Types:\n')
+
+
+                # indexes desired 'types' in the txt file (for enumerated type)
                 j = 0
-                # indexes desired 'types'
                 for types_num in data.json()['types']:
                     j += 1
                     name_type = types_num['type']['name']
                     file.write(f'   {j}> {name_type}\n')
+
+
                 if repeater() == 'N':
-                    file.close
+                    file.close # Closes file and loop if user inputs 'N', else repeats
                     break
 
-# takes return value of main and repeats if Y
 
+
+# takes return value of main and repeats if Y
 def repeater():
     while True:
         try:
